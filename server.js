bdd022cec176059f4d83599c5d5259d43635c337
Priyanka Sonawane <priyankasonawane7666@gmail.com>
2025-03-06 12:26:31 +0530

    adding all bakend files to github

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-const PORT = 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log("🚀 Server is running on port: " + PORT);
 });
